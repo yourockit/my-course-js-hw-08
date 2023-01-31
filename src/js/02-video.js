d0859408d73f10b92f4c48cec878d593174472ef
Yourockit <sts2ro@gmail.com>
2023-01-31 15:15:51 +0300

    js-hw-8-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const CURRENT_TIME = 'current-time';
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(e) {
+    const playTime = e.seconds;
+    localStorage.setItem(CURRENT_TIME, playTime);
+};
+
+function setCurrentTime() {
+    const getCurrentTime = localStorage.getItem(CURRENT_TIME);
+    if (getCurrentTime) {
+        player.setCurrentTime(getCurrentTime).catch(function(error) {
+            console.log(error);
+        });
+    };
+};
+
+setCurrentTime();
