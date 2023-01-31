93f91604d2b5b3fc63b30296f29b7c1e9f7cca19
Yourockit <sts2ro@gmail.com>
2023-01-31 15:28:06 +0300

    my-js-hw-08-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-const CURRENT_TIME = 'current-time';
+const CURRENT_TIME = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
