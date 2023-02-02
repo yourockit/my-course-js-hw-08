f838aee0b3ac89884966dfdfe186b831bd0d7afd
Yourockit <sts2ro@gmail.com>
2023-02-02 17:31:08 +0300

    my-js-hw-08-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormChange, 500));
+
+const USER_DATA = 'feedback-form-state';
+const formData = {};
+
+populateInput();
+
+function onFormChange(e) {
+    formData[e.target.name] = e.target.value;
+    const formDataJSON = JSON.stringify(formData);
+    localStorage.setItem(USER_DATA, formDataJSON);
+};
+
+function populateInput() {
+    const userDataJSON = localStorage.getItem(USER_DATA);
+    const userData = JSON.parse(userDataJSON);
+    if (userData) {
+        Object.entries(userData).forEach(([key, value]) => {
+            refs.form.elements[key].value = value;
+            formData[key] = value;
+        });
+    };
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+    e.currentTarget.reset();
+    localStorage.removeItem(USER_DATA);
+
+};
